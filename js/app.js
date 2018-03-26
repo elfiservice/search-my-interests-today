f169890f361d1489078a638aba3fd92b55b260c1
Armando Jr <elfiservice@hotmail.com>
2018-03-26 08:29:41 -0300

    first commit using fetch Obj!

--- js/app.js
+++ js/app.js
@@ -9,15 +9,13 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
 
-        $.ajax({
-            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
             headers: {
                 Authorization: 'Client-ID 795f815f9db950af216b40bc64a9bdfd4a5805d5387f88c8fa5d6a88a682aa31'
             }
-        }).done(addImage)
-            .fail(function (error) {
-                requestError(error, 'image');
-            });
+        }).then(response => response.json())
+            .then(addImage)
+            .catch(err => requestError(err, 'image'));
 
         function addImage(images) {
             let htmlResult = '';
@@ -36,18 +34,15 @@
             responseContainer.insertAdjacentHTML('afterbegin', htmlResult);
         }
         //Request related articles by NYT API
-        $.ajax({
-            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=5ed067b15e7c4deeb029547379606584`,
-        }).done(addArticle)
-            .fail(function (error) {
-                requestError(error, 'articles');
-            });
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=5ed067b15e7c4deeb029547379606584`)
+            .then(response => response.json())
+            .then(addArticle)
+            .catch(err => requestError(err, 'articles'));
 
         //if sucess
         function addArticle(articles) {
             let htmlResult = '';
             if (articles && articles.response.docs && articles.response.docs[0]) {
-                console.log(articles.response.docs[0]);
                 const articlesData = articles.response.docs;
                 htmlResult = '<ul>' + articlesData.map(article =>
                     `<li class="article">
